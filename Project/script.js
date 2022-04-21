ea26ac4fb93bd9c667fc2ade4e5ae0b1a7db5231
DenisSK228 <9293817575den@gmail.com>
2022-04-22 00:52:50 +0700

    Try to do something with db (fail)

--- Project/script.js
+++ Project/script.js
@@ -2,6 +2,7 @@ const express = require('express');
 const nunjucks = require("nunjucks")
 const app = express();
 const path = require('path');
+const sqlite = require('sqlite3')
 
 nunjucks.configure(path.resolve(__dirname, 'templates'), {
     autoescape: true,
@@ -23,7 +24,32 @@ app.get('/signup', function (req, res) {
 
     res.render("Signup.html")
 
+});
+app.get("/index", function (req, res) {
+    res.render("index.hbs")
+});
+
+let db = new sqlite.Database("autor.db", (err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log("DB Open...");
+    }
+});
+
+db.serialize(function () {
+    let query = "SELECT * FROM autor";
+    db.each(query, function (err, row) {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log(row);
+        }
+    })
 })
+
+
+db.close();
 
 app.listen(7344, function () {
     console.log("'Spotify free' started work");
